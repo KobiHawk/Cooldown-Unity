ec2ca79a907776f759cce1a73862be8a89f7f948
KobiHawk <kasaiaisu1@hotmail.com>
2017-11-23 18:54:00 -0600

    Moving Cooldown project from c++ to c# with Unity

---
+++ BulletController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+
+
+    public int speed = 10;
+    public int replicateDelay = 200;
+    Rigidbody2D rb;
+    public int replicateTimer = 0; // used to prevent lenses from being placed too close, time in frames until bullet can replicate again
+    //private float refractionIndex = 1.33f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if(replicateTimer > 0)
+        {
+            replicateTimer--;
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+        else if ((collision.gameObject.tag == "Lens") && (replicateTimer == 0))
+        {
+            Vector2 normal = getNormal(rb.velocity);
+
+            BulletController newBullet = Instantiate(this) as BulletController;
+
+            rb.velocity = new Vector2(rb.velocity.normalized.x + normal.x * 0.1f, rb.velocity.normalized.y + normal.y * 0.1f);
+            newBullet.rb.velocity = new Vector2(rb.velocity.normalized.x + normal.x * -0.1f, rb.velocity.normalized.y + normal.y * -0.1f);
+
+            Vector2 temp = rb.velocity.normalized;
+            rb.velocity = Vector2.zero;
+            rb.AddForce(temp * speed);
+
+            temp = newBullet.rb.velocity.normalized;
+            newBullet.rb.velocity = Vector2.zero;
+            newBullet.rb.AddForce(temp * speed);
+
+
+            replicateTimer = replicateDelay;
+            newBullet.replicateTimer = replicateDelay;
+        }
+    }
+
+    /*
+     * Called by PlayerController.cs in the fire() function
+     */
+    public void beginMovement(Vector3 direction)
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * speed);
+    }
+
+    //Finds the normal (perpendicular) line of the vector, used to skew trajectory of projectile velocity when passing through lens
+    private Vector2 getNormal(Vector2 vector)
+    {
+        Vector2 result = vector;
+        result.x = result.y;
+        result.y = -vector.x;
+
+        return result.normalized;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+    enum Abilities { Shoot, Line, Accelerate };
+
+    private Rigidbody2D rb;
+    private Abilities currAbility = Abilities.Shoot;
+
+    public int speed = 10;
+    public int fireDelay = 10; // number of frames between shots;
+    public int accelerateDuration = 200; // number of frames accelerate lasts
+    public int accelerateStrength = 2; // amount that accelerate speeds your fire rate
+    private int currFireDelay = 0;
+    private int currAccelerateDuration = 0;
+
+    public BulletController bullet;
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D lineHitbox;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //lineRenderer = GetComponent<LineRenderer>();
+    }
+
+	void FixedUpdate ()
+    {
+        move();      
+    }
+
+    void Update()
+    {
+        checkForInput();
+        if(currAbility == Abilities.Shoot && Input.GetButton("Fire1") && currFireDelay == 0)
+        {
+            fire();
+            currFireDelay = fireDelay;
+        }
+        else if (Input.GetButtonDown("Fire1"))
+        {
+            switch(currAbility)
+            {
+                case Abilities.Line:
+                    StartCoroutine("drawLine");
+                    break;
+
+                    /*
+                case Abilities.Accelerate:
+                    fireDelay /= accelerateStrength;
+                    break;
+                    */ // we want to use this ability as soon as it's pressed
+            }
+        }
+        if(currFireDelay > 0)
+        {
+            currFireDelay--;
+        }
+        if(currAccelerateDuration > 0)
+        {
+            currAccelerateDuration--;
+            if(currAccelerateDuration == 0)
+            {
+                fireDelay *= accelerateStrength;
+            }
+        }
+    }
+
+    void move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(transform.right * moveHorizontal * speed);
+        rb.AddForce(transform.up * moveVertical * speed);
+    }
+
+    void fire()
+    {
+        BulletController newBullet = Instantiate(bullet, transform.position, Quaternion.identity) as BulletController;
+
+        Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = (Input.mousePosition - sp).normalized;
+        newBullet.beginMovement(dir);
+    }
+
+    private void checkForInput()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            if (currAbility != Abilities.Line)
+            {
+                currAbility = Abilities.Line;
+            }
+            else // if we already had that ability selected, cancel it, go back to default shooting
+            {
+                currAbility = Abilities.Shoot;
+            }
+        }
+        if(Input.GetKeyDown("2"))
+        {
+            fireDelay /= accelerateStrength;
+            currAccelerateDuration = accelerateDuration;
+        }
+    }
+
+    /*
+     * On mouse click, set the start of the line
+     * Then, while the mouse is held, continue drawing a line to the mouse cursor
+     * When the button is released, set the end of the line and exit the loop
+     */
+    IEnumerator drawLine()
+    {
+        bool mouseIsHeld = true;
+
+        Vector3 v3 = Input.mousePosition;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        v3.z = -2;
+        Vector2 firstPoint = new Vector2(v3.x, v3.y);
+
+        lineRenderer.SetPosition(0, v3);
+
+        while (mouseIsHeld)
+        {
+            v3 = Input.mousePosition;
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            v3.z = -2;
+
+            lineRenderer.SetPosition(1, v3);
+            if (Input.GetButtonUp("Fire1"))
+            {
+                //update hitbox for line
+                Vector2[] newHitbox;
+                newHitbox = lineHitbox.points;
+                newHitbox[0] = firstPoint;
+                newHitbox[1] = new Vector2(v3.x, v3.y);
+                lineHitbox.points = newHitbox;
+
+                mouseIsHeld = false;
+                currAbility = Abilities.Shoot; // After placing a line, the ability should go on cooldown
+            }
+            yield return null;
+        }
+    }
+}
